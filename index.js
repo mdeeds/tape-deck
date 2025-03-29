d7f9a089da3118f866e59fd6e15a22e826c52867
mdeeds <mdeeds@gmail.com>
2025-03-29 16:15:44 -0700

    Small improvements to reverb

--- index.js
+++ index.js
@@ -137,29 +137,26 @@ class CanvasCircle extends EventTarget {
 
 class Monitor {
   constructor(inputSource) {
+    console.log('Setting up monitor.');
     this.audioContext = inputSource.context;
 
-    this.predelay = this.audioContext.createDelay(1.0);
-    this.predelay.delayTime.value = 0.05;
-    inputSource.connect(this.predelay);
+    this.inputGain = this.audioContext.createGain();
+    inputSource.connect(this.inputGain);
+    this.inputGain.gain.value = 1.0;
 
     const kernelSize = 0.1;
-    const halflife = 1.0;  // The halflife of the reverb in seconds.
+    const halflife = 0.2;  // The halflife of the reverb in seconds.
+    // Reverb kernel and convolution
     const kernel = this._kernel(kernelSize, halflife);
-
     this.convolver = this.audioContext.createConvolver();
     // Important: turn off normalization so we don't get a ringing effect.
     this.convolver.normalize = false;
     this.convolver.buffer = kernel;
-    this.predelay.connect(this.convolver);
-    this.convolver.connect(this.audioContext.destination);
 
-    this.loopback = this.audioContext.createDelay(kernelSize);
-    this.loopback.delayTime.value = kernelSize;
-    this.predelay.connect(this.loopback);
-
+    // Loopback
+    this.loopbackDelay = this.audioContext.createDelay(kernelSize);
+    this.loopbackDelay.delayTime.value = kernelSize;
     this.loopbackGain = this.audioContext.createGain();
-
     // g ^ (haflife / kernelSize) = 0.5
     // (haflife / kernelSize) ln g = ln(0.5)
     // ln g = ln(0.5) / (halflife / kernelSize)
@@ -167,9 +164,42 @@ class Monitor {
     // g = pow(0.5, (halflife / kernelSize))
     this.loopbackGain.gain.value = Math.pow(0.5, kernelSize / halflife);
 
-    this.loopback.connect(this.loopbackGain);
-    this.loopbackGain.connect(this.convolver);
-    this.loopbackGain.connect(this.loopback);
+    //
+    // inputSource ------------------------------------------> convolver -> output
+    //               ^                                    /
+    //               \                                   /
+    //                 loopback delay <- loopback gain <-
+    this.inputGain.connect(this.convolver);
+    this.convolver.connect(this.audioContext.destination);
+
+    this.inputGain.connect(this.loopbackGain);
+    this.loopbackGain.connect(this.loopbackDelay);
+    this.loopbackDelay.connect(this.loopbackGain);
+    this.loopbackDelay.connect(this.convolver);
+
+    this.testTone = this.audioContext.createOscillator();
+    this.testTone.type = 'sine';
+    this.testTone.frequency.value = 440;
+    this.testGain = this.audioContext.createGain();
+    this.testGain.gain.value = 0;
+    this.testTone.start();
+    this.testTone.connect(this.testGain);
+    this.testGain.connect(this.inputGain);
+    document.body.addEventListener('keydown', (event) => {
+      if (event.code === 'Backquote') {
+        console.log('boop');
+        this.testGain.gain.value = 0.05;
+      }
+    });
+    document.body.addEventListener('keyup', (event) => {
+      if (event.code === 'Backquote') {
+        console.log('unboop');
+        this.testGain.gain.value = 0.0;
+      }
+    });
+
+    inputSource.connect(this.audioContext.destination);
+    console.log('Monitor connected.');
   }
 
   // Returns an AudioBuffer filled with noise of the specified duration.
@@ -178,18 +208,53 @@ class Monitor {
     const frameCount = Math.round(sampleRate * durationSeconds);
     const buffer = this.audioContext.createBuffer(1, frameCount, sampleRate);
     const channelData = buffer.getChannelData(0);
+    let g = 1.0;
+    const decayRate = Math.pow(0.5, halflife / sampleRate);
     for (let i = 0; i < frameCount; i++) {
-      const t = i / sampleRate;
-      const g = Math.pow(0.5, t / halflife);
-      // channelData[i] = Math.random() * 2 - 1;
-      if (Math.random() < 0.01) {
+      if (Math.random() < 0.1) {
         channelData[i] = 0.1 * g * (2 * Math.random() - 1);
       } else {
         channelData[i] = 0;
       }
+      g *= decayRate;
     }
+    this._lowPassFilter(channelData);
+    this._normalize(channelData);
     return buffer;
+
   }
+
+  // Applies a low pass filter to the buffer with a simple IIR model
+  _lowPassFilter(channelData) {
+    const sampleRate = this.audioContext.sampleRate;
+    const alpha = 0.1;
+    let lastValue = 0;
+    for (let pass = 0; pass < 2; ++pass) {
+      // Do two passes so that the 'lastValue' from the first pass can be used in the
+      // second pass.
+      for (let i = 0; i < channelData.length; i++) {
+        const value = channelData[i];
+        channelData[i] = alpha * lastValue + (1 - alpha) * value;
+        lastValue = channelData[i];
+      }
+    }
+  }
+
+  // Normalizes the buffer so the maximum value is 1.0 (or -1.0)
+  _normalize(channelData) {
+    let max = 0;
+    for (let i = 0; i < channelData.length; i++) {
+      const value = Math.abs(channelData[i]);
+      if (value > max) {
+        max = value;
+      }
+    }
+    const scale = max > 0 ? 1.0 / max : 1.0;
+    for (let i = 0; i < channelData.length; i++) {
+      channelData[i] *= scale * 0.05;
+    }
+  }
+
 }
 
 class Main {
